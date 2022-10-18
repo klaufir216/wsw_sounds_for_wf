4f2ac4f3c3ab85f90c88fe3e3a7e568b08caf36d
klaufir216 <klaufir@hotmail.com>
2022-10-18 20:07:36 +0200

    add installer for soundmod

---
+++ WarforkSoundmodInstaller/MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace WarforkSoundmod
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.tbLogs = new System.Windows.Forms.TextBox();
+            this.btnUninstall = new System.Windows.Forms.Button();
+            this.btnCopyLogs = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(12, 12);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(75, 23);
+            this.btnInstall.TabIndex = 0;
+            this.btnInstall.Text = "Install";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_click);
+            // 
+            // tbLogs
+            // 
+            this.tbLogs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbLogs.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLogs.Location = new System.Drawing.Point(12, 41);
+            this.tbLogs.Multiline = true;
+            this.tbLogs.Name = "tbLogs";
+            this.tbLogs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbLogs.Size = new System.Drawing.Size(776, 397);
+            this.tbLogs.TabIndex = 1;
+            // 
+            // btnUninstall
+            // 
+            this.btnUninstall.Location = new System.Drawing.Point(93, 12);
+            this.btnUninstall.Name = "btnUninstall";
+            this.btnUninstall.Size = new System.Drawing.Size(75, 23);
+            this.btnUninstall.TabIndex = 2;
+            this.btnUninstall.Text = "Uninstall";
+            this.btnUninstall.UseVisualStyleBackColor = true;
+            this.btnUninstall.Click += new System.EventHandler(this.btnUninstall_Click);
+            // 
+            // btnCopyLogs
+            // 
+            this.btnCopyLogs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopyLogs.Location = new System.Drawing.Point(597, 12);
+            this.btnCopyLogs.Name = "btnCopyLogs";
+            this.btnCopyLogs.Size = new System.Drawing.Size(191, 23);
+            this.btnCopyLogs.TabIndex = 3;
+            this.btnCopyLogs.Text = "Copy logs to clipboard";
+            this.btnCopyLogs.UseVisualStyleBackColor = true;
+            this.btnCopyLogs.Click += new System.EventHandler(this.btnCopyLogs_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCopyLogs);
+            this.Controls.Add(this.btnUninstall);
+            this.Controls.Add(this.tbLogs);
+            this.Controls.Add(this.btnInstall);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Warfork Soundmod Installer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.TextBox tbLogs;
+        private System.Windows.Forms.Button btnUninstall;
+        private System.Windows.Forms.Button btnCopyLogs;
+    }
+}
+
---
+++ WarforkSoundmodInstaller/MainForm.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.Threading;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace WarforkSoundmod
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InvokeUI(Action a)
+        {
+            try
+            {
+                this.BeginInvoke(new MethodInvoker(a));
+            }
+            catch (System.InvalidOperationException)
+            {
+                // swallow InvalidOperationException:
+                // happens when closing window and InvokeUI is called.
+            }
+
+        }
+
+
+        private void AddLogLine(string line)
+        {
+            InvokeUI(() =>
+            {
+                tbLogs.Text += line + "\r\n";
+                tbLogs.SelectionStart = tbLogs.Text.Length;
+                tbLogs.ScrollToCaret();
+
+            });
+        }
+
+        private bool IsWarforkRunning()
+        {
+            return Process.GetProcesses().Where(p => p.ProcessName == "warfork_x64").Count() > 0;
+        }
+
+        private string getBasewfUserDir()
+        {
+            var userHome = Environment.GetEnvironmentVariable("USERPROFILE");
+            var basewfDir = Path.Combine(userHome, @"Documents\My Games\Warfork 2.1\basewf");
+            return basewfDir;
+        }
+
+        private void Install()
+        {
+            try
+            {
+
+                var basewfDir = getBasewfUserDir();
+
+                if (!Directory.Exists(basewfDir))
+                {
+
+                    Directory.CreateDirectory(basewfDir);
+                    AddLogLine($"Created ${basewfDir}");
+                }
+
+                var zipPath = Path.Combine(basewfDir, "sounds.zip");
+
+                File.WriteAllBytes(zipPath, Data.sounds);
+                using (var zipfile = ZipFile.OpenRead(zipPath))
+                {
+                    foreach (var entry in zipfile.Entries)
+                    {
+                        var path = Path.Combine(basewfDir, entry.FullName.Replace("/", "\\"));
+                        if (path.EndsWith("\\")
+                            && !Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+
+                        if (!path.EndsWith("\\"))
+                        {
+                            AddLogLine($"Extrtact: {path}");
+                            entry.ExtractToFile(path, overwrite: true);
+                        }
+                    }
+                }
+
+                try { File.Delete(zipPath); } catch { };
+
+                var origBackupPath = $"{wfExePath}.original";
+                if (!File.Exists(origBackupPath))
+                {
+                    File.Copy(wfExePath, origBackupPath);
+                    AddLogLine($"Created backup of original exe: {origBackupPath}");
+                }
+
+                File.WriteAllBytes(wfExePath, Data.warfork_x64);
+                AddLogLine($"Installed patched exe: {wfExePath}");
+
+
+            }
+            catch (Exception ex)
+            {
+                AddLogLine(ex.ToString());
+            }
+        }
+
+        string steamPath;
+        string wfExePath;
+        Thread installThread;
+
+        bool prereqChecks()
+        {
+            if (IsWarforkRunning())
+            {
+                AddLogLine("Exit Warfork before installing.");
+                return false;
+            }
+
+            steamPath = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Valve\Steam", "InstallPath", "")?.ToString();
+            if (steamPath is null)
+            {
+                AddLogLine("Could not find steam install path.");
+                return false;
+            }
+
+            wfExePath = Path.Combine(steamPath, @"steamapps\common\fvi\fvi-launcher\applications\warfork\Warfork.app\Contents\Resources\warfork_x64.exe");
+            if (!File.Exists(wfExePath))
+            {
+                AddLogLine($"Could not find warfork_x64.exe at: {wfExePath}");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnInstall_click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbLogs.Clear();
+
+                if (!prereqChecks())
+                    return;
+
+                installThread = new Thread(() => Install());
+                installThread.Start();
+
+            }
+            catch (Exception ex)
+            {
+                AddLogLine(ex.ToString());
+            }
+        }
+
+        private void btnUninstall_Click(object sender, EventArgs e)
+        {
+            tbLogs.Clear();
+            try
+            {
+                if (!prereqChecks())
+                    return;
+
+                var origBackupPath = $"{wfExePath}.original";
+                if (File.Exists(origBackupPath))
+                {
+                    File.Copy(origBackupPath, wfExePath, overwrite: true);
+                    AddLogLine("Restored original exe.");
+                }
+
+                var basewfDir = getBasewfUserDir();
+                var soundsDir = Path.Combine(basewfDir, "sounds");
+                if (Directory.Exists(soundsDir))
+                {
+                    var zipPath = Path.Combine(basewfDir, "sounds.zip");
+
+                    File.WriteAllBytes(zipPath, Data.sounds);
+                    List<string> directories = new List<string>();
+                    using (var zipfile = ZipFile.OpenRead(zipPath))
+                    {
+                        foreach (var entry in zipfile.Entries)
+                        {
+                            var path = Path.Combine(basewfDir, entry.FullName.Replace("/", "\\"));
+                            if (path.EndsWith("\\"))
+                            {
+                                directories.Add(path);
+                            }
+
+                            if (!path.EndsWith("\\")
+                                && File.Exists(path))
+                            {
+
+                                AddLogLine($"Delete: {path}");
+                                File.Delete(path);
+                            }
+                        }
+                    }
+                    var sortedDirs = directories.OrderBy(elem => elem.Count(c => c == '\\')).Reverse().ToList();
+                    foreach (var dir in sortedDirs)
+                    {
+
+                        try
+                        {
+                            if (Directory.Exists(dir))
+                            {
+                                AddLogLine($"Delete: {dir}");
+                                Directory.Delete(dir);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            AddLogLine($"  {ex.Message}");
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                AddLogLine(ex.ToString());
+            }
+        }
+
+        private void btnCopyLogs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(tbLogs.Text);
+            }
+            catch { }
+
+        }
+    }
+}
